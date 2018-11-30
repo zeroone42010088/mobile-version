c5618ccbfc5b410f3a3aac8c4d3af25d7aeedbb1
Глеб <stroikagoda@yandex.ru>
2018-11-30 09:27:56 +0400

    Готовая форма связи с разделением на два вида

--- www/remontservo.ru/js/cat_contactform.js
+++ www/remontservo.ru/js/cat_contactform.js
@@ -19,5 +19,30 @@
     }
     http.send(new FormData(f));
   }, false);
- 
+
+});
+console.log('txmar');
+
+$(function() {
+  console.log(777)
+  document.getElementById('ajax-contact-form1').addEventListener('submit', function(evt){
+    console.log(555)
+    var http = new XMLHttpRequest(), f = this;
+    var th = $(this);
+    evt.preventDefault();
+    http.open("POST", "js/contact.php", true);
+    http.onreadystatechange = function() {
+      if (http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+        if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
+          th.trigger("reset");
+        }
+      }
+    }
+    http.onerror = function() {
+      alert('Ошибка, попробуйте еще раз');
+    }
+    http.send(new FormData(f));
+  }, false);
+
 });
