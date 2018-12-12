1bb411d2eda45dfbed00b417c4a743ef1e8da33c
Глеб <stroikagoda@yandex.ru>
2018-12-12 17:51:42 +0400

    Загружаю картинки выполненных работ на сервер

---
+++ www/remontservo.ru/glebwebsite.ru/old/js/contact.php
@@ -0,0 +1,50 @@
+<?php
+if (isset ($_POST['contactFF'])) {
+  $to = "114polaris@gmail.com";//"114polaris@gmail.com";
+  $from = "new_client@remontservo.ru";
+  $subject = "Заполнена контактная форма на сайте ".$_SERVER['HTTP_REFERER'];
+$message = "\nEmail".$_POST['contactFF'].
+    "\nТелефон".$_POST['telFF'].
+    "\nТип, модель неисправного блока: ".$_POST['nameFF'].
+    "\nОписание неисправности. номер ошибки: ".$_POST['projectFF'].
+    "\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
+ 
+  $boundary = md5(date('r', time()));
+  $filesize = '';
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "From: " . $from . "\r\n";
+  $headers .= "Reply-To: " . $from . "\r\n";
+  $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+  $message="
+Content-Type: multipart/mixed; boundary=\"$boundary\"
+ 
+--$boundary
+Content-Type: text/plain; charset=\"utf-8\"
+Content-Transfer-Encoding: 7bit
+ 
+$message";
+     if(is_uploaded_file($_FILES['fileFF']['tmp_name'])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['fileFF']['tmp_name'])));
+         $filename = $_FILES['fileFF']['name'];
+         $filetype = $_FILES['fileFF']['type'];
+         $filesize = $_FILES['fileFF']['size'];
+         $message.="
+ 
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+ 
+$attachment";
+     }
+   $message.="
+--$boundary--";
+ 
+  if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
+    mail($to, $subject, $message, $headers);
+    echo 'Ваша заявка по блоку, ', $_POST['nameFF'].', отправлена, спасибо!';
+  } else {
+    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
+  }
+}
+?>
---
+++ www/remontservo.ru/glebwebsite.ru/old/js/sliderslick.js
@@ -0,0 +1,16 @@
+$(document).on('ready', function() {
+
+            $(".regular").slick({
+                аccessibility: true,
+                autoplay:true,
+                arrows:true,
+                focusOnSelect:true,
+                pauseOnHover: true,
+                dots: false,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            });
+
+
+        });
