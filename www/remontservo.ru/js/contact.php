4704a5df5739a221b37a1393482497921f9556a5
Глеб <stroikagoda@yandex.ru>
2018-12-13 17:12:18 +0400

    Загрузил ещё картинки в выполненные работы
    - на главное странице есть ошибки валидации
    - добавил чат на главную страницу и в контакты
    - загрузил картинки в выполненные работы

--- www/remontservo.ru/js/contact.php
+++ www/remontservo.ru/js/contact.php
@@ -1,11 +1,16 @@
 <?php
 if (isset ($_POST['contactFF'])) {
-  $to = "msulianov@gmail.com";
+  $to = "msulianov@gmail.com";//"114polaris@gmail.com";
   $from = "new_client@remontservo.ru";
   $subject = "Заполнена контактная форма на сайте ".$_SERVER['HTTP_REFERER'];
-$message = "\nEmail:   ".$_POST['contactFF'].
-    "\nТелефон:   ".$_POST['telFF'].
-    "\nТип, модель неисправного блока: ".$_POST['nameFF'].
+$message = "\nEmail".$_POST['contactFF'].
+    "\nТелефон".$_POST['telFF'].
+    "\nТип, модель неисправного блока: ".$_POST['nameFF'];
+
+    if(isset($_POST['nameFF1']) and $_POST['nameFF1']!= ''):
+      $message .= "\nТип/модель неисправного чпу, либо контроллера с панелью оператора".$_POST['nameFF1'];
+    endif;
+$message .=
     "\nОписание неисправности. номер ошибки: ".$_POST['projectFF'].
     "\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
  
@@ -37,12 +42,46 @@ Content-Disposition: attachment; filename=\"$filename\"
  
 $attachment";
      }
+
+
+          if(is_uploaded_file($_FILES['fileFF2']['tmp_name'])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['fileFF2']['tmp_name'])));
+         $filename = $_FILES['fileFF2']['name'];
+         $filetype = $_FILES['fileFF2']['type'];
+         $filesize = $_FILES['fileFF2']['size'];
+         $message.="
+ 
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+ 
+$attachment";
+     }
+
+
+
+          if(is_uploaded_file($_FILES['fileFF3']['tmp_name'])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['fileFF3']['tmp_name'])));
+         $filename = $_FILES['fileFF3']['name'];
+         $filetype = $_FILES['fileFF3']['type'];
+         $filesize = $_FILES['fileFF3']['size'];
+         $message.="
+ 
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+ 
+$attachment";
+     }
    $message.="
 --$boundary--";
  
   if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
     mail($to, $subject, $message, $headers);
     echo 'Ваша заявка по блоку, ', $_POST['nameFF'].', отправлена, спасибо!';
+    //echo $message;
   } else {
     echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
   }
